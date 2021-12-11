ce1e439ea34c271ff02f23e21c0adeb260b765b1
Joel Zeta <zetajoel2911@gmail.com>
2021-12-11 09:26:35 -0500

    desafio4

---
+++ src/ItemListDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemListDetails() {
+    return (
+        <div>
+             <h1>soy el detalle</h1>
+        </div>
+    )
+}
+
+export default ItemListDetails
